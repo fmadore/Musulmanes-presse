11679066abdae462b62dc7a86808e384d5ca6f5c
fmadore <frederick_madore@outlook.com>
2024-04-04 12:56:39 +0200

    Visualisations par pays

--- 8. analyse_sentiments.py
+++ 8. analyse_sentiments.py
@@ -3,9 +3,9 @@ from textblob import TextBlob
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Charger le fichier CSV depuis GitHub
+# Charger le fichier CSV depuis GitHub, en incluant une colonne pour les pays
 url = 'https://raw.githubusercontent.com/fmadore/Musulmanes-presse/master/preprocessed_corpus.csv'
-df = pd.read_csv(url, usecols=['Processed_Content'])
+df = pd.read_csv(url, usecols=['Processed_Content', 'Pays'])  # Assurez-vous que 'Pays' est le nom correct de la colonne
 
 # Analyse des sentiments
 def analyse_sentiment(text):
@@ -13,26 +13,33 @@ def analyse_sentiment(text):
         analysis = TextBlob(text)
         return pd.Series([analysis.sentiment.polarity, analysis.sentiment.subjectivity])
     except:
-        return None
+        return pd.Series([None, None])
 
 df[['Polarity', 'Subjectivity']] = df['Processed_Content'].apply(analyse_sentiment)
 
 sns.set(style="whitegrid")
 
-# Histogramme des scores de polarité
-plt.figure(figsize=(10, 6))
-sns.histplot(df['Polarity'], bins=30, kde=False, color='skyblue')
-plt.title('Distribution des scores de polarité')
-plt.xlabel('Polarité')
-plt.ylabel('Nombre de textes')
-plt.savefig('distribution_polarité.png')
-plt.show()
+# Fonction pour tracer les graphiques par pays
+def plot_sentiment_by_country(country):
+    df_country = df[df['Pays'] == country]
+    # Histogramme des scores de polarité
+    plt.figure(figsize=(10, 6))
+    sns.histplot(df_country['Polarity'], bins=30, kde=False, color='skyblue')
+    plt.title(f'Distribution des scores de polarité - {country}')
+    plt.xlabel('Polarité')
+    plt.ylabel('Nombre de textes')
+    plt.savefig(f'distribution_polarité_{country}.png')
+    plt.show()
 
-# Nuage de points de polarité vs subjectivité
-plt.figure(figsize=(10, 6))
-sns.scatterplot(x='Polarity', y='Subjectivity', data=df, alpha=0.5, edgecolor=None, color='red')
-plt.title('Polarité vs Subjectivité')
-plt.xlabel('Polarité')
-plt.ylabel('Subjectivité')
-plt.savefig('polarité_vs_subjectivité.png')
-plt.show()
+    # Nuage de points de polarité vs subjectivité
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='Polarity', y='Subjectivity', data=df_country, alpha=0.5, edgecolor=None, color='red')
+    plt.title(f'Polarité vs Subjectivité - {country}')
+    plt.xlabel('Polarité')
+    plt.ylabel('Subjectivité')
+    plt.savefig(f'polarité_vs_subjectivité_{country}.png')
+    plt.show()
+
+# Générer les visualisations pour chaque pays
+for country in df['Pays'].unique():
+    plot_sentiment_by_country(country)
