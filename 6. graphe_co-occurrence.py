33269f97b1229dae8ca23f6e424f6796cd365bae
fmadore <frederick_madore@outlook.com>
2024-04-04 10:29:09 +0200

    Update

--- 6. graphe_co-occurrence.py
+++ 6. graphe_co-occurrence.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from collections import defaultdict
 import re
-import matplotlib.pyplot as plt
+from pyvis.network import Network
 import networkx as nx
 
 # Charger les données
@@ -30,8 +30,6 @@ co_occurrence = defaultdict(int)
 # Définir la fenêtre pour la co-occurrence
 window_size = 5
 
-# Calculer les co-occurrences en excluant les paires de mots identiques
-# et en combinant les paires dans les deux sens (homme-femme et femme-homme)
 for content in df['Processed_Content']:
     for i in range(len(content)):
         target_word = content[i]
@@ -40,44 +38,36 @@ for content in df['Processed_Content']:
         for j in range(start, end):
             if i != j:
                 co_occurred_word = content[j]
-                # S'assurer que les mots ne sont pas identiques
                 if target_word != co_occurred_word:
-                    # Ordonner les mots alphabétiquement pour combiner les paires dans les deux sens
                     ordered_pair = tuple(sorted((target_word, co_occurred_word)))
                     co_occurrence[ordered_pair] += 1
 
-# Transformer le dictionnaire de co-occurrence en DataFrame
-co_occurrence_df = pd.DataFrame(list(co_occurrence.items()), columns=['Word Pair', 'Co-occurrence'])
-co_occurrence_df['Word1'] = co_occurrence_df['Word Pair'].apply(lambda x: x[0])
-co_occurrence_df['Word2'] = co_occurrence_df['Word Pair'].apply(lambda x: x[1])
-co_occurrence_df.drop('Word Pair', axis=1, inplace=True)
-
-# Filtrer pour ne garder que les paires de mots avec une co-occurrence significative
-seuil_co_occurrence = 50  # Ajustez ce seuil selon vos besoins
-co_occurrence_df_filtré = co_occurrence_df[co_occurrence_df['Co-occurrence'] > seuil_co_occurrence]
-
-# Créer un graphe à partir des paires de mots filtrées
+# Créer un graphe NetworkX à partir des données de co-occurrence
 G = nx.Graph()
 
-# Ajouter des arêtes au graphe basées sur le DataFrame filtré
-for index, row in co_occurrence_df_filtré.iterrows():
-    word1, word2, co_occurrence = row['Word1'], row['Word2'], row['Co-occurrence']
-    G.add_edge(word1, word2, weight=co_occurrence)
+# Ajouter des arêtes basées sur le dictionnaire de co-occurrence
+for (word1, word2), weight in co_occurrence.items():
+    if weight > 50:  # Filtrer par un seuil de co-occurrence si nécessaire
+        G.add_edge(word1, word2, weight=weight)
 
-# Filtrer les nœuds isolés qui peuvent apparaître après l'application de la liste d'exclusion
-G.remove_nodes_from(list(nx.isolates(G)))
+# Initialiser PyVis Network
+nt = Network('800px', '100%', notebook=True)
+nt.force_atlas_2based(gravity=-50, central_gravity=0.01, spring_length=100, spring_strength=0.02, damping=0.4, overlap=0)
 
-# Dessiner le graphe
-plt.figure(figsize=(12, 12))
-pos = nx.kamada_kawai_layout(G)  # Utiliser Kamada-Kawai layout
+# Ajouter les nœuds et les arêtes du graphe NetworkX au graphe PyVis
+for node in G.nodes:
+    nt.add_node(node, title=node)
 
-edges = G.edges(data=True)
-weights = [edata['weight'] / 50 for _, _, edata in edges]  # Ajustement pour des arêtes plus fines
+max_weight = max([edata['weight'] for _, _, edata in G.edges(data=True)])
+min_width, max_width = 0.5, 5.0  # Définissez ces valeurs selon vos préférences
 
-nx.draw_networkx_edges(G, pos, width=weights, edge_color='grey')
-nx.draw_networkx_labels(G, pos, font_size=12)
-plt.title('Graphe de Co-occurrence')
-plt.axis('off')
-plt.savefig('graphe_co-occurrence.png', format='png', dpi=300, bbox_inches='tight')
-plt.show()
+for source, target, edata in G.edges(data=True):
+    weight = edata['weight']
+    width = min_width + (max_width - min_width) * (weight / max_weight)  # Normaliser la largeur
+    nt.add_edge(source, target, width=width)
 
+# Activer la physique pour un placement initial optimisé des nœuds
+nt.toggle_physics(True)
+
+# Générer et ouvrir la visualisation interactive
+nt.show('co_occurrence_graph.html')
