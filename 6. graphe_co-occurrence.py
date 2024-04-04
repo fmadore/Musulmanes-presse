0834403f62e6454730c72611e13425273bdcfdc0
fmadore <frederick_madore@outlook.com>
2024-04-04 10:17:27 +0200

    Update

--- 6. graphe_co-occurrence.py
+++ 6. graphe_co-occurrence.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from collections import defaultdict
 import re
 import matplotlib.pyplot as plt
@@ -9,15 +8,17 @@ import networkx as nx
 url = 'https://raw.githubusercontent.com/fmadore/Musulmanes-presse/master/preprocessed_corpus.csv'
 df = pd.read_csv(url)
 
+# Définir la liste des mots à exclure
+exclusion_list = ['El', 'aujourd', 'hui', 'à', 'il', 't', 'el', 'non', 'Faso', 'Burkina', 'mme']
+
 # Fonction pour nettoyer et convertir les données en listes Python
 def clean_and_convert_to_list(text):
     if isinstance(text, str):
-        # Utiliser une expression régulière pour trouver tous les mots (séparés par des espaces)
         words = re.findall(r'\b\w+\b', text)
-        # Retourner la liste des mots
+        # Filtrer les mots en utilisant la liste d'exclusion
+        words = [word for word in words if word not in exclusion_list]
         return words
     else:
-        # Si ce n'est pas une chaîne de caractères, retourner tel quel
         return text
 
 # Appliquer la fonction de nettoyage à la colonne 'Processed_Content'
@@ -51,27 +52,32 @@ co_occurrence_df['Word1'] = co_occurrence_df['Word Pair'].apply(lambda x: x[0])
 co_occurrence_df['Word2'] = co_occurrence_df['Word Pair'].apply(lambda x: x[1])
 co_occurrence_df.drop('Word Pair', axis=1, inplace=True)
 
-# Filtrer le DataFrame pour ne garder que les paires de mots avec une co-occurrence significative
+# Filtrer pour ne garder que les paires de mots avec une co-occurrence significative
 seuil_co_occurrence = 50  # Ajustez ce seuil selon vos besoins
 co_occurrence_df_filtré = co_occurrence_df[co_occurrence_df['Co-occurrence'] > seuil_co_occurrence]
 
 # Créer un graphe à partir des paires de mots filtrées
 G = nx.Graph()
 
-# Ajouter des arêtes au graphe
+# Ajouter des arêtes au graphe basées sur le DataFrame filtré
 for index, row in co_occurrence_df_filtré.iterrows():
     word1, word2, co_occurrence = row['Word1'], row['Word2'], row['Co-occurrence']
     G.add_edge(word1, word2, weight=co_occurrence)
 
-# Dessiner le graphe sans afficher les nœuds
+# Filtrer les nœuds isolés qui peuvent apparaître après l'application de la liste d'exclusion
+G.remove_nodes_from(list(nx.isolates(G)))
+
+# Dessiner le graphe
 plt.figure(figsize=(12, 12))
-pos = nx.spring_layout(G, k=2.5)  # Utiliser spring_layout pour la mise en page
-nx.draw_networkx_edges(G, pos, width=0.5, edge_color='grey')  # Dessiner uniquement les arêtes
-nx.draw_networkx_labels(G, pos, font_size=14)  # Dessiner les étiquettes des nœuds avec une taille de police plus grande
+pos = nx.kamada_kawai_layout(G)  # Utiliser Kamada-Kawai layout
+
+edges = G.edges(data=True)
+weights = [edata['weight'] / 50 for _, _, edata in edges]  # Ajustement pour des arêtes plus fines
+
+nx.draw_networkx_edges(G, pos, width=weights, edge_color='grey')
+nx.draw_networkx_labels(G, pos, font_size=12)
 plt.title('Graphe de Co-occurrence')
-
-# Sauvegarder la figure
+plt.axis('off')
 plt.savefig('graphe_co-occurrence.png', format='png', dpi=300, bbox_inches='tight')
-
-# Afficher le graphe
 plt.show()
+
